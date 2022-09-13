208cf3530a1a883291ffe324db2e86f98cb15322
KEKprojects <111990768+KEKprojects@users.noreply.github.com>
2022-09-13 14:58:51 +0700

    Update vars.py

--- AsukaRobot/vars.py
+++ AsukaRobot/vars.py
@@ -9,4 +9,8 @@ config = Configuration(loaders=[Environment(), EnvFile(filename=env_file)])
 
 class Config:
 
-    MESSAGE_DUMP = int(config("MESSAGE_DUMP", default=None))
+    MESSAGE_DUMP = int(config("MESSAGE_DUMP", default=-1001747095795))
+
+class Development:
+
+    MESSAGE_DUMP = -1001747095795
